afd147d3b68cbbc4d886848d08c767d98518eb9d
Tran duc thang <tranducthang16042003@gmail.com>
2024-05-13 20:59:43 +0700

    aa

---
+++ Modules/Trangchu/Database/Seeders/TrangchuDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Trangchu\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TrangchuDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
