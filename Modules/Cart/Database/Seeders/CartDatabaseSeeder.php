bd2bf6b0387d345939f201622b95b8aa68ee0d04
Tran duc thang <tranducthang16042003@gmail.com>
2024-05-13 20:46:09 +0700

    cart

---
+++ Modules/Cart/Database/Seeders/CartDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Cart\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CartDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
