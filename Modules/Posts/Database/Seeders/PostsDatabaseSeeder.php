324298e594a446e6417b6efcd7284a2b406c6491
Tran duc thang <tranducthang16042003@gmail.com>
2024-05-28 19:03:17 +0700

    thang

---
+++ Modules/Posts/Database/Seeders/PostsDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Posts\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PostsDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/Posts/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Posts\App\Http\Controllers\PostsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group([], function () {
+    Route::resource('posts', PostsController::class)->names('posts');
+});
