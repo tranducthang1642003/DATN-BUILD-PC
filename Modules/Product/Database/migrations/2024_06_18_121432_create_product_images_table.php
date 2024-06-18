a8faa95f725624db00565b132287e8e1fccb0ef4
Tran duc thang <tranducthang16042003@gmail.com>
2024-06-18 22:04:00 +0700

    update datebase

---
+++ Modules/Product/Database/migrations/2024_06_18_121432_create_product_images_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products');
+            $table->string('image_url');
+            $table->boolean('is_main')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_images');
+    }
+};
