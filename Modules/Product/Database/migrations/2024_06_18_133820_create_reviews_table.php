e3666b3483cb9e24e8e7ea9e5131c0e5c0ae1120
anhdtp123 <137995891+anhdtp123@users.noreply.github.com>
2024-07-02 21:19:49 +0700

    Review

--- Modules/Product/Database/migrations/2024_06_18_133820_create_reviews_table.php
+++ Modules/Product/Database/migrations/2024_06_18_133820_create_reviews_table.php
@@ -13,9 +13,14 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('user_id')->nullable();
             $table->integer('rating');
             $table->text('comment')->nullable();
             $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
         });
     }
 
---
+++ Modules/Review/Database/Seeders/ReviewDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Review\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ReviewDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
