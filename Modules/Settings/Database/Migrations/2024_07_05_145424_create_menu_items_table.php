c8ba740554060cc7b354bed9f7da87ab86b0df9d
Thao IT <thaorom19999@gmail.com>
2024-07-05 21:56:42 +0700

    database setting

---
+++ Modules/Settings/Database/Migrations/2024_07_05_145424_create_menu_items_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuItemsTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('menu_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('menu_items');
+    }
+}
