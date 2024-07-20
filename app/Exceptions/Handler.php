07d1a43af4517533afe1f1b7deb26cab0c046375
thang <tranducthang@gmail.com>
2024-07-20 19:30:03 +0700

    404

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -27,4 +27,13 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function render($request, Throwable $exception)
+{
+    if ($exception instanceof NotFoundHttpException) {
+        return response()->view('errors.404', [], 404);
+    }
+
+    return parent::render($request, $exception);
+}
 }
