53e939208b1c3950099f0a6b2f851acead209b08
anhdtp123 <137995891+anhdtp123@users.noreply.github.com>
2024-08-05 19:39:57 +0700

    Buildpc

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -55,8 +55,14 @@
                         <ion-icon name="chevron-forward-outline" class="chevron-icon"></ion-icon>
                     </button>
                     <a class="w-full button-none" id="product_list" href="{{ route('product') }}"><button class="button_sidebar_cl "><ion-icon name="caret-forward-outline" class="mr-3"></ion-icon>Danh sách</button></a>
-                    <a class="w-full button-none" id="product_list" href="{{ route('admin.buildpc') }}"><button class="button_sidebar_cl "><ion-icon name="caret-forward-outline" class="mr-3"></ion-icon>Build PC</button></a>
                     <a class="w-full button-none" id="product_add" href="{{ route('add_product') }}"><button class="button_sidebar_cl "><ion-icon name="caret-forward-outline" class="mr-3"></ion-icon>Thêm</button></a>
+                </div>
+                <div class="">
+                    <button id="buildpc" class="button_sidebar buildpc_active">
+                        <p class=""><ion-icon class="icon_sidebar" name="bag-outline"></ion-icon>Build PC</p>
+                        <ion-icon name="chevron-forward-outline" class="chevron-icon"></ion-icon>
+                    </button>
+                    <a class="w-full button-none" id="buildpc_list" href="{{ route('admin.buildpc') }}"><button class="button_sidebar_cl "><ion-icon name="caret-forward-outline" class="mr-3"></ion-icon>Danh sách</button></a>
                 </div>
                 <div class="">
                     <button id="category" class="button_sidebar category_active">
