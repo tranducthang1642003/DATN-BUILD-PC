e33073b31585e0c8434b9aa4c36352acc8d7efdb
Thao IT <thaorom19999@gmail.com>
2024-08-21 18:37:40 +0700

    blog

--- Modules/Promotions/routes/web.php
+++ Modules/Promotions/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Promotions\App\Http\Controllers\admin\PromotionController;
+use Modules\Promotions\App\Http\Controllers\admin\promotionControllerAdmin;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,10 +15,10 @@ use Modules\Promotions\App\Http\Controllers\admin\PromotionController;
 */
 
 Route::group(['middleware' => 'admin'], function () {
-    Route::get('admin/vouchers', [PromotionController::class, 'index'])->name('vouchers.index');
-    Route::get('admin/vouchers/create', [PromotionController::class, 'create'])->name('vouchers.create');
-    Route::post('admin/vouchers', [PromotionController::class, 'store'])->name('vouchers.store');
-    Route::get('admin/vouchers/{id}/edit', [PromotionController::class, 'edit'])->name('vouchers.edit');
-    Route::post('admin/vouchers/{id}', [PromotionController::class, 'update'])->name('vouchers.update');
-    Route::delete('admin/vouchers/delete/{id}', [PromotionController::class, 'destroy'])->name('vouchers.destroy');
+    Route::get('admin/vouchers', [promotionControllerAdmin::class, 'index'])->name('vouchers.index');
+    Route::get('admin/vouchers/create', [promotionControllerAdmin::class, 'create'])->name('vouchers.create');
+    Route::post('admin/vouchers', [promotionControllerAdmin::class, 'store'])->name('vouchers.store');
+    Route::get('admin/vouchers/{id}/edit', [promotionControllerAdmin::class, 'edit'])->name('vouchers.edit');
+    Route::post('admin/vouchers/{id}', [promotionControllerAdmin::class, 'update'])->name('vouchers.update');
+    Route::delete('admin/vouchers/delete/{id}', [promotionControllerAdmin::class, 'destroy'])->name('vouchers.destroy');
 });
