be378153278ceb2ff703b2d2cca3819796b43d68
tranducthang <tranducthang.uway@gmail.com>
2024-08-25 08:51:51 +0700

    momo

--- resources/views/public/checkout.blade.php
+++ resources/views/public/checkout.blade.php
@@ -7,8 +7,8 @@
                 <div class="border-b-2 p-2 mb-4 border-yellow-500">
                     <h1 class="text-3xl font-bold text-orange-600">Thông tin khách hàng</h1>
                 </div>
-                <form id="order-form" action="{{ route('orders.placeOrder') }}" method="POST" class="mt-8">
-                @csrf
+                <form action="{{ route('orders.placeOrder') }}" method="POST" class="mt-8">
+                    @csrf
                     <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
                         <div class="p-1">
                             <label for="full-name" class="block text-lg font-medium text-gray-700">Họ và tên</label>
@@ -36,7 +36,7 @@
                         </div>
                         <div class="p-1">
                             <label for="note" class="block text-lg font-medium text-gray-700">Ghi chú</label>
-                            <textarea id="note" name="note" class="mt-1 p-2 border border-gray-300 rounded-md w-full" rows="3" placeholder="Nhập ghi chú"></textarea>
+<textarea id="note" name="note" class="mt-1 p-2 border border-gray-300 rounded-md w-full" rows="3" placeholder="Nhập ghi chú"></textarea>
                         </div>
                     </div>
                     <div class="mt-4 border-b-2 border-yellow-500 p-2">
@@ -48,20 +48,17 @@
                             <span class="ml-2 text-lg font-medium text-gray-700">Thanh toán bằng tiền mặt</span>
                         </label>
                         <label class="inline-flex items-center mb-2">
-                            <input type="radio" id="payment_url" name="payment-method" value="momo" class="form-radio h-4 w-4 lg:h-5 lg:w-5 text-yellow-500" required>
-                            <input type="hidden" name="total" value="{{$total}}">
+                            <input type="radio" name="payment-method" value="momo" class="form-radio h-4 w-4 lg:h-5 lg:w-5 text-yellow-500" required>
                             <span class="ml-2 text-lg font-medium text-gray-700">Thanh toán bằng MoMo</span>
                         </label>
                         <label class="inline-flex items-center mb-2">
-                            
-                            <input type="radio" id="payment_url" name="payment-method" value="pay" class="form-radio h-4 w-4 lg:h-5 lg:w-5 text-yellow-500" required>
+                            <input type="radio" name="payment-method" value="vnpay" class="form-radio h-4 w-4 lg:h-5 lg:w-5 text-yellow-500" required>
                             <span class="ml-2 text-lg font-medium text-gray-700">Thanh toán bằng VNPay</span>
                         </label>
                     </div>
                     <div class="mt-8">
                         <button type="submit" class="bg-yellow-400 text-white px-4 py-2 rounded hover:bg-yellow-500">Xác nhận đặt hàng</button>
                     </div>
-                    
                 </form>
             </div>
         </div>
@@ -84,7 +81,7 @@
 
                 
                 <div class="border-t mt-4 pt-4">
-                    <div class="flex justify-between items-center">
+<div class="flex justify-between items-center">
                         <h2 class="text-xl font-bold text-orange-600">Tổng tiền</h2>
                         <span class="text-xl font-bold">{{ number_format($total) }} VNĐ</span>
                     </div>
@@ -95,25 +92,3 @@
 </div>
 
 @include('public.footer.footer')
-
-
-
-<script>
-document.querySelector('#order-form').addEventListener('submit', function(event) {
-    var paymentMethod = document.querySelector('input[name="payment-method"]:checked').value;
-    var form = event.target;
-    
-    if (paymentMethod === 'momo') {
-        event.preventDefault(); 
-        
-       
-        console.log("Form Data:", new FormData(form));
-        
-        var paymentUrl = "{{ route('momo') }}"; 
-        form.action = paymentUrl;
-        form.submit();
-    }
-});
-
-
-</script>
